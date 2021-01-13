3935970c4cccb96ac15e79fc3774d13e5e58ecac
HubTicaBB <tijana.ilic87@protonmail.com>
2021-01-13 09:33:53 +0100

    Write login tests & login custom command

--- e2etests/cypress/integration/projectTest.spec.js
+++ e2etests/cypress/integration/projectTest.spec.js
@@ -1,5 +1,76 @@
-describe('Test suite', () => {
+/// <reference types="Cypress" />
+
+beforeEach(() => {
+    cy.visit('localhost');
+})
+
+describe('Test suite', () => 
+{
     it('Test that localhost is accesible', () => {
         cy.visit("http://localhost");
     })
+
+    it('Login as User', () => {
+        cy.get('#username')
+          .type('User');
+
+        cy.get('#password')
+          .type('User');
+
+        cy.get('.btn')
+          .click();
+
+        cy.location('pathname')
+          .should('eq', '/blog.html')
+          .should(() => {
+              expect(localStorage.getItem('usernameLogedIn')).to.eq('User');
+              expect(localStorage.getItem('userStatus')).to.eq('0');
+          });
+
+        cy.get('#add-new-blogpost-button')
+          .should('not.be.visible');
+
+        cy.get('#create-blog-form')
+          .should('not.be.visible');
+    });
+
+    it('Login as Admin', () => {
+        cy.get('#username')
+          .type('Admin');
+
+        cy.get('#password')
+          .type('Admin');
+
+        cy.get('.btn')
+          .click();
+
+        cy.location('pathname')
+          .should('eq', '/blog.html')
+          .should(() => {
+              expect(localStorage.getItem('usernameLogedIn')).to.eq('Admin');
+              expect(localStorage.getItem('userStatus')).to.eq('1');
+          });
+
+        cy.get('#add-new-blogpost-button')
+          .should('be.visible');
+
+        cy.get('#create-blog-form')
+          .should('not.be.visible');
+    });
+
+    it('Write a new blogpost', () => {
+        cy.login('Admin', 'Admin');
+
+        cy.get('#add-new-blogpost-button')
+          .click();
+
+        cy.get('#title')
+          .type('New Blog Title');
+
+        cy.get('#text')
+          .type('New blog text...');
+
+        cy.get('.btn-success')
+          .click();
+    });
 })
--- e2etests/cypress/support/commands.js
+++ e2etests/cypress/support/commands.js
@@ -23,3 +23,16 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.visit('localhost');
+
+    cy.get('#username')
+    .type(username);
+
+  cy.get('#password')
+    .type(password);
+
+  cy.get('.btn')
+    .click();
+});
